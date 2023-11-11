b8281f24947f3bb11613c302621430469e93cf3e
congntph33455 <congthathu8@gmail.com>
2023-11-11 22:35:08 +0700

    cong1

---
+++ duanmau1-main/admin/index.php
@@ -0,0 +1,58 @@
+<?php
+include "header.php";
+//controller
+if (isset($_GET['act'])) {
+   $act = $_GET['act'];
+   switch ($act) {
+      case 'addtheloai':
+
+         break;
+      case 'addphim':
+
+         break;
+      case 'dstk':
+
+         break;
+      case 'dsbl':
+
+         break;
+      case 'thongketk':
+
+         break;
+         case 'thongketk':
+
+            break;
+            case 'thongketk':
+
+               break;
+               case 'thongketk':
+
+                  break;
+
+      default:
+         # code...
+         break;
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+include "home.php";
+include "footer.php";
+
+
+
+
+?>
